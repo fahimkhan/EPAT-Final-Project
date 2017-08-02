b491eab5188b85c42b658b0ffbfea25ab576abc3
Fahim <khanfahim2000@yahoo.com>
2017-08-03 00:19:24 +0530

    Modified the backtesting design

--- Strategis/backtest.py
+++ Strategis/backtest.py
@@ -1,49 +1,87 @@
 
-
-from abc import ABCMeta, abstractmethod
-
-
-class Strategy(object):
-	"""Strategy is an abstract base class providing an interface for
-	all subsequent (inherited) trading strategies.
-	The goal of a (derived) Strategy object is to output a list of signals,
-	which has the form of a time series indexed pandas DataFrame.
-	In this instance only a single symbol/instrument is supported."""
-
-	__metaclass__ = ABCMeta
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
 
 
-	@abstractmethod
-	def generate_signals(self):
-		"""An implementation is required to return the DataFrame of symbols 
-		containing the signals to go long, short or hold (1, -1 or 0)."""
-		raise NotImplementedError("Should implement generate_signals()!")
+
+data_location = "../Data"
+
+class FinancialData(object):
+	"""docstring for FinancialData"""
+	def __init__(self, symbol):
+		self.symbol = symbol
+		self.get_data()
+
+	def get_data(self):
+		#skip additional row with unwanted header name
+		self.data = pd.read_csv("../Data/"+self.symbol+"-EQ.csv",index_col='Date',names=['Date','Open','High','Low','Close','Volume'],skiprows=1) 
+	
+	def plot_data(self, cols=['Close']):
+		self.data[cols].plot(figsize=(10, 6))
+		plt.show()
 
 
-class Portfolio(object):
-	"""An abstract base class representing a portfolio of 
-	positions (including both instruments and cash), determined
-	on the basis of a set of signals provided by a Strategy."""
 
-	__metaclass__ = ABCMeta
+class BacktestBase(FinancialData):
+	def __init__(self,symbol,start,end,amount,ftc=0.0,ptc=0.0):
+		FinancialData.__init__(self,symbol)
+		self.start = start
+		self.end = end
+		self.amount = amount
+		self.initial_amount = amount
+		self.ftc = ftc
+		self.ptc = ptc
+		self.units = 0
+		self.trades = 0
+		self.position = 0
 
-	@abstractmethod
-	def generate_positions(self):
-		"""Provides the logic to determine how the portfolio 
-		positions are allocated on the basis of forecasting
-		signals and available cash."""
-		raise NotImplementedError("Should implement generate_positions()!")
+	def print_balance(self,date=''):
+		print("%s |Current balance is %9.2f " %(date,self.amount))
 
-	@abstractmethod
-	def backtest_portfolio(self):
-		"""Provides the logic to generate the trading orders
-		and subsequent equity curve (i.e. growth of total equity),
-		as a sum of holdings and cash, and the bar-period returns
-		associated with this curve based on the 'positions' DataFrame.
+	def get_date_price(self,bar):
+		date = str(self.run.index[bar])[:10]
+		price = self.run['Close'].ix[bar]
+		return date,price
 
-		Produces a portfolio object that can be examined by 
-		other classes/functions."""
-		raise NotImplementedError("Should implement backtest_portfolio()!")
+	def place_buy_order(self,bar,units=None,amount=None):
+		date,price = self.get_date_price(bar)
+		if units is None:
+			units = math.floor(amount/price) #include ftc and ptc
+
+		self.amount -= (units*price)*(1+self.ptc)+self.ftc
+		self.units += units
+		self.trades += 1
+		print("%s |Buying  %4d  units  at  %8.2f "%(date,units,price))
+		self.print_balance(date)
+
+	def place_sell_order(self,bar,units=None,amount=None):
+		date,price = self.get_date_price(bar)
+		if units is None:
+			units = math.floor(amount/price) #include ftc and ptc
+
+		self.amount += (units*price)*(1-self.ptc)-self.ftc
+		self.units -= units
+		self.trades += 1
+		print("%s |Selling  %4d  units  at  %8.2f "%(date,units,price))
+		self.print_balance(date)
+
+	def close_out(self,bar):
+		date,price = self.get_date_price(bar)
+		self.amount += (self.units*price) #Include ftc and ptc ?
+		print(50 * '=')
+		print ("%s |buying/selling %d units at %7.2f"%(date,self.units,price))
+		print("Final balance [$]: %8.2f"%self.amount)
+		perf = (self.amount-self.initial_amount)/self.initial_amount *100
+		print("Performace [%%]:%8.2f"%perf)
+		print("#Trades :%d"%self.trades)
+
+
+
+
+# objFinancialData = FinancialData("SBIN")
+# objFinancialData.plot_data()
 
 
 
--- Strategis/moving_average.py
+++ Strategis/moving_average.py
@@ -1,26 +1,13 @@
 import pandas as pd
+import numpy as np
+
+from backtest import BacktestBase
 
 
-from backtest import Strategy, Portfolio
-
-
-
-class MovingAverageStrategy(Strategy):
-	def __init__(self,symbol,bars,sma_window,lma_window):
-		self.symbol =symbol
-		self.bars = bars
-		self.sma_window = sma_window
-		self.lma_window  = lma_window
-
-
-
-
-if __name__ == "__main__":
-
-	symbol = "SBIN"
-	bars = pd.read_csv("../Data/"+symbol+"-EQ.csv",index_col='Date',names=['Date','Open','High','Low','Close','Volume'],skiprows=1) #skip additional row with unwanted header name
-
-	print bars.head()
+class MovingAverageStrategy(BacktestBase):
+	def __init__(self):
+		print
+	
 
 
 
