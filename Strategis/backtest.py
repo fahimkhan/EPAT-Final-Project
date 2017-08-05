ae41b144d422efa2b755335a6fcd25ee0c19fcab
Fahim <khanfahim2000@yahoo.com>
2017-08-05 23:11:34 +0530

    Simple Moving Average Completed

--- Strategis/backtest.py
+++ Strategis/backtest.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import pandas as pd
+import math
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
@@ -25,10 +26,11 @@ class FinancialData(object):
 
 
 class BacktestBase(FinancialData):
-	def __init__(self,symbol,start,end,amount,ftc=0.0,ptc=0.0):
+	# def __init__(self,symbol,start,end,amount,ftc=0.0,ptc=0.0):
+	def __init__(self,symbol,amount,ftc=0.0,ptc=0.0):	
 		FinancialData.__init__(self,symbol)
-		self.start = start
-		self.end = end
+		# self.start = start
+		# self.end = end
 		self.amount = amount
 		self.initial_amount = amount
 		self.ftc = ftc
@@ -38,11 +40,12 @@ class BacktestBase(FinancialData):
 		self.position = 0
 
 	def print_balance(self,date=''):
-		print("%s |Current balance is %9.2f " %(date,self.amount))
+		pass
+		#print("%s |Current balance is %9.2f " %(date,self.amount))
 
 	def get_date_price(self,bar):
-		date = str(self.run.index[bar])[:10]
-		price = self.run['Close'].ix[bar]
+		date = str(self.data_run.index[bar])[:10]
+		price = self.data_run['Close'].ix[bar]
 		return date,price
 
 	def place_buy_order(self,bar,units=None,amount=None):
@@ -53,7 +56,7 @@ class BacktestBase(FinancialData):
 		self.amount -= (units*price)*(1+self.ptc)+self.ftc
 		self.units += units
 		self.trades += 1
-		print("%s |Buying  %4d  units  at  %8.2f "%(date,units,price))
+		#print("%s |Buying  %4d  units  at  %8.2f "%(date,units,price))
 		self.print_balance(date)
 
 	def place_sell_order(self,bar,units=None,amount=None):
@@ -64,14 +67,14 @@ class BacktestBase(FinancialData):
 		self.amount += (units*price)*(1-self.ptc)-self.ftc
 		self.units -= units
 		self.trades += 1
-		print("%s |Selling  %4d  units  at  %8.2f "%(date,units,price))
+		#print("%s |Selling  %4d  units  at  %8.2f "%(date,units,price))
 		self.print_balance(date)
 
 	def close_out(self,bar):
 		date,price = self.get_date_price(bar)
 		self.amount += (self.units*price) #Include ftc and ptc ?
-		print(50 * '=')
-		print ("%s |buying/selling %d units at %7.2f"%(date,self.units,price))
+		#print(50 * '=')
+		#print ("%s |buying/selling %d units at %7.2f"%(date,self.units,price))
 		print("Final balance [$]: %8.2f"%self.amount)
 		perf = (self.amount-self.initial_amount)/self.initial_amount *100
 		print("Performace [%%]:%8.2f"%perf)
--- Strategis/moving_average.py
+++ Strategis/moving_average.py
@@ -5,8 +5,47 @@ from backtest import BacktestBase
 
 
 class MovingAverageStrategy(BacktestBase):
-	def __init__(self):
-		print
+	def run(self,SMA1,SMA2):
+		msg = 'Running SMA strategy for %s |SMA1 =%d |SMA2 = %d |ftc = %f|ptc = %f'
+		msg = msg%(self.symbol,SMA1,SMA2,self.ftc,self.ptc)
+		#print(msg)
+		#print(50 * '=')
+		#Re initialization
+		self.position = 0
+		self.amount = self.initial_amount
+		self.trades = 0
+
+		#Data Preparation
+		self.data_run = self.data.copy()
+		self.data_run['SMA1'] = self.data_run['Close'].rolling(SMA1).mean()
+		self.data_run['SMA2'] = self.data_run['Close'].rolling(SMA2).mean()
+		self.data_run.dropna(inplace=True)
+
+		for bar in range(len(self.data_run)):
+			if self.position == 0:
+				if self.data_run['SMA1'].ix[bar] > self.data_run['SMA2'].ix[bar]:
+					self.place_buy_order(bar,amount=self.amount) #Check for ftc and ptc ?
+					self.position = 1 #Take Position
+			elif self.position == 1:
+				if self.data_run['SMA1'].ix[bar] < self.data_run['SMA2'].ix[bar]:
+					self.place_sell_order(bar,units=self.units)
+					self.position = 0   #Market nuetral
+
+		self.close_out(bar)
+
+
+
+
+symbolList = ["HDFCBANK","ICICIBANK","KOTAKBANK","ONGC","INFY","RELIANCE","HDFC","LT","IOC","SBIN","HINDUNILVR",
+"MARUTI","ITC","TCS"]
+
+for symbol in symbolList:
+	sma = MovingAverageStrategy(str(symbol),10000)
+	sma.run(50,250)
+
+
+
+
 	
 
 
